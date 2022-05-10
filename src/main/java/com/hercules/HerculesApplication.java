1c5b076971548ee9434149ea1ada7e610270e8ac
dsvlk <dsds@mail.ru>
2022-05-10 13:16:05 +0300

    The project has been created

---
+++ src/main/java/com/hercules/HerculesApplication.java
@@ -0,0 +1,13 @@
+package com.hercules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerculesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerculesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hercules/HerculesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hercules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerculesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
